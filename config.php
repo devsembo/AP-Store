3c644f2b3ce9634ce18b8edd74751916d0863a97
Anderson Pedro <113034804+ApDev0@users.noreply.github.com>
2022-09-08 03:22:45 +0100

    Add files via upload

---
+++ config.php
@@ -0,0 +1,29 @@
+<?php
+
+define('APP_NAME',          'APStore');
+define('APP_VERSION',       '2k22');
+
+define('BASE_URL',          'http://localhost/APStore/public/');
+
+define('STATUS',            ['PENDENTE','EM PROCESSAMENTO','ENVIADA','CANCELADA','CONCLUIDA']);
+
+
+// MYSQL
+define('MYSQL_SERVER',      'localhost');
+define('MYSQL_DATABASE',    'apstorebd');
+define('MYSQL_USER',        'apstorebd_user');
+define('MYSQL_PASS',        'Qi3A6ax1su3ug3Va686Irom4Q44efa');
+define('MYSQL_CHARSET',     'utf8');
+
+// AES encriptação
+define('AES_KEY',           'qs8BzdLD8N7qJgqJ3qmGsuh8HMhCWqG4');
+define('AES_IV',            'WSzH6HcdZAYdQ9be');
+
+// mail
+define('EMAIL_HOST',        'mail.m87.pt');
+define('EMAIL_FROM',        'cliente@m87.pt');
+define('EMAIL_PASS',        'Skills_001.');
+define('EMAIL_PORT',        587);
+
+// pdf
+define('PDF_PATH',      "./pdfs");
